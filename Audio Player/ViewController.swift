6cecfbd5348303ae910d471d0f150027f7f42845
Kostyantyn Runduyev <krunduev@curiousit.eu>
2016-11-22 22:05:04 +0200

    Added functionality

--- Audio Player/ViewController.swift
+++ Audio Player/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player = AVAudioPlayer()
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBAction func playPressed(_ sender: Any) {
+        player.play()
+    }
+    
+    @IBAction func pausePressed(_ sender: Any) {
+        player.pause()
+    }
+    
+    @IBAction func sliderMoved(_ sender: Any) {
+        player.volume = slider.value
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let audioPath = Bundle.main.path(forResource: "007", ofType: "mp3")
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+        } catch {
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
